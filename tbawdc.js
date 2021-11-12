0c49c420dc6f2655b1e79cf9e0d5379d192bcbbe
irwinsnet <stacy.k.irwin@gmail.com>
2021-11-12 14:22:34 -0800

    Added feature to store TBA authentication key.

--- tbawdc.js
+++ tbawdc.js
@@ -5,11 +5,20 @@
     tbaConnector.init = function(initCallback) {
         tableau.authType = tableau.authTypeEnum.custom
 
+        // Hide the data selection controls if only authentication is needed
         if (tableau.phase == tableau.phaseEnum.authPhase) {
             $("#interactive").hide();
         } else {
             $("#interactive").show();
         }
+
+        // Check browser storate for authorization key
+        var locStore = localStorage;
+        var auth_key = locStore.getItem("TBA_Auth_Key")
+        if (auth_key) {
+            $("#auth_key").val(auth_key)
+        }
+
         initCallback()
     };
 
@@ -28,10 +37,21 @@
     $(document).ready(function () {
         $("#submitButton").click(function () {
             // Authentication
-            tableau.password = $("#auth_key").val()
-            tableau.log(`Password: ${tableau.password}`)
+            tableau.password = $("#auth_key").val().trim();
+            tableau.log(`Password: ${tableau.password}`);
 
-            // Interactive
+            // Store authorization key in browser
+            if ($("#store_key").prop("checked")) {
+                var msg
+                msg = "Only store authentication keys on secure computers. " +
+                      "Do you want to store the authentication key?";
+                if (confirm(msg)) {
+                    var locStore = localStorage
+                    locStore.setItem("TBA_Auth_Key", tableau.password)
+                }
+            };
+
+            // Save data from interactive phase
             if (tableau.phase == tableau.phaseEnum.interactivePhase) {
                 tableau.connectionName = "The Blue Alliance Read API V3";
                 tba_data = JSON.stringify({
